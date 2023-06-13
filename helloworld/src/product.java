46c0a7962e21d040262dcdbcf3717f52bf5b1115
aimanalishezan <aimanalishezan22@gamil.com>
2023-06-13 23:22:04 +0600

    pr

---
+++ helloworld/src/product.java
@@ -0,0 +1,10 @@
+public class product {
+    public static void main(String[] args) throws Exception{
+        System.out.println("id:602832");
+        System.out.println("tittle:iphone");
+        System.out.println("price:1 cr");
+        System.out.println("description:perfect product");
+        System.out.println("catagory:phone");
+
+    }
+}

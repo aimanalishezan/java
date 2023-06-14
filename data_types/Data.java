684e5eab1544caec83396c5302f40ad4b1f21db5
aimanalishezan <aimanalishezan22@gamil.com>
2023-06-14 22:50:52 +0600

    D

---
+++ data_types/Data.java
@@ -0,0 +1,40 @@
+/*data types in java
+There are mainly two types of data
+1.premitive
+2.nonpremtive
+
+***primitive:
+1.boolean----keyword--boolean
+2.numaric
+
+**numaric:
+1.character----keyword--char
+2.integral
+
+**integral:
+1.integer
+2.floatin-point
+
+**integer:
+1.Byte---keyword--byte
+2.Short---keyword--short
+3.Int---keyword--int
+4.Long---keyword--long
+
+**floating-point
+1.Float---keyword--float
+2.Double---keyword--double
+
+***non_primitive:
+1.string
+2.arry
+3.e.t.c
+ */
+
+
+public class Data{
+    public static void main(String[]args)throws Exception{
+        System.out.println("THATS ALL ARE DATA TYPES IN JAVA LANGUAGE");
+    }
+    
+}

adb67925999016c55192b0fc17bd9e315158c394
aimanalishezan <aimanalishezan22@gamil.com>
2023-06-14 21:50:12 +0600

    escape

---
+++ helloworld/src/escapesequence.java
@@ -0,0 +1,22 @@
+/*
+escape sequences in java are
+\t------tab
+\n------newline
+\r------carrige return
+\b------backspace
+\"------double quote
+\'------single quote
+\\------backslash
+*/
+
+
+    public class escapesequence {
+
+        public static void main(String[]args) {
+
+            System.out.println("pgw");//use println to create a new line after print
+            System.out.print("aiman \njannat \nmim\n");
+            System.out.print("1\t2 \n3\t4\n");
+        }
+        
+    }

2470b3a02032f38c84c35fffcc6be3cf68c80e30
aimanalishezan <aimanalishezan22@gamil.com>
2023-06-14 22:19:00 +0600

    n

--- java tutorial/src/first.java
+++ java tutorial/src/first.java
@@ -1,3 +1,13 @@
+/*
+five phase of java
+1.edit
+2.compile
+3.load
+4.verify
+5.exicute
+this is how a java programme works on our computer
+*/
+
 public class first {
     public static void main(String[] args) throws Exception {
         System.out.println("aiman ali shezan");

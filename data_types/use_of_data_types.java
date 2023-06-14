15db61909a7a134ef7581c6f2c767b3f0c0d12aa
aimanalishezan <aimanalishezan22@gamil.com>
2023-06-14 23:23:13 +0600

    uses

--- data_types/use_of_data_types.java
+++ data_types/use_of_data_types.java
@@ -1,8 +1,19 @@
+//in java first you need to decleared the data type of variables
 public class use_of_data_types {
     public static void main(String[]args){
-        int age=19;
-        String name=
+        int age=19;//dynamic initialization.when you declared the type of variable and at the same time add the value of it
+        String name="aiman";
+        boolean male=true;
+        char name1='a';
+        float size=5.4f;//use f with the float value so that compiler can understood
+        double age1=18.5;
 
-        System.out.println(null);
+        //System.out.println("Age is="+age+"\nName="+name+"\nMale="+male+"\nHer name="+name1+"\nHer size="+size+"\nHer age="+age1);//you can print all in one line by this method
+        System.out.println("Age="+age);
+        System.out.println("Name="+name);
+        System.out.println("Male="+male);
+        System.out.println("Her name="+name1);//use +variable_name to print the value of that variable
+        System.out.println("Her size="+size);
+        System.out.println("Her Age="+age1);
     }
 }

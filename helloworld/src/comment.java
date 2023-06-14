089e28460931c81f83ddd7c4bfb24ac2af0c8ad3
aimanalishezan <aimanalishezan22@gamil.com>
2023-06-14 21:58:01 +0600

    comment

---
+++ helloworld/src/comment.java
@@ -0,0 +1,16 @@
+//this is single line comment
+/*
+ * and
+ * this
+ * is
+ * multeline
+ * comment
+ */
+
+
+public class comment {
+    public static void main(String[]args){
+        System.out.println("this how you can use comment in your code on java");
+    }
+    
+}

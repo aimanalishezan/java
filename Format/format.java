f851c6bc7b9430f03ba80f1b84eb597470cbb9a7
aimanalishezan <aimanalishezan22@gamil.com>
2023-06-15 20:17:12 +0600

    f

--- Format/format.java
+++ Format/format.java
@@ -17,7 +17,7 @@ public class format {
         double birth=12.2;
         String name="aiman";
         
-        System.out.printf("age=%d\n",age);
+        System.out.printf("age=%d\n",age);//use printf method for format specifier
         System.out.printf("group=%c\n",group);
         System.out.printf("degree=%.2f\n",degree);//use .2/3f enter the number you wants to print after the point
         System.out.printf("birth=%.2f\n",birth);

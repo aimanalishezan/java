5feee05e8aa007d1fe3e881f02135aac2d316b59
aimanalishezan <aimanalishezan22@gamil.com>
2023-09-08 23:16:43 +0600

    dam

---
+++ switch/digitspelling.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class digitspelling {
+    public static void main(String[] args) {
+        int number;
+        System.out.print("Enter any digit:");
+        Scanner num= new Scanner(System.in);
+        number=num.nextInt();
+        switch (number){
+            case 0:
+                System.out.println("one");
+            break;
+            case 2:
+                System.out.println("two");
+            break;
+            case 3:
+                System.out.println("three");
+            break;
+            case 4:
+                System.out.println("four");
+            break;
+            case 5:
+                System.out.println("five");
+            break;
+            case 6:
+                System.out.println("six");
+            break;
+            case 7:
+                System.out.println("seven");
+            break;
+            case 8:
+                System.out.println("eight");
+            break;
+            case 9:
+                System.out.println("nine");
+            break;
+            default:
+                System.out.println("not a digit");
+        }
+        num.close();
+    }
+}

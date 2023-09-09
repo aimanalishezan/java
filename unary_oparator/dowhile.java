1d8e1abdedc67905e33da0dfcb5a938885436c99
aimanalishezan <aimanalishezan22@gamil.com>
2023-09-09 19:37:38 +0600

    dowh

---
+++ unary_oparator/dowhile.java
@@ -0,0 +1,9 @@
+public class dowhile {
+    public static void main(String[] args) {
+        int i=2;
+        do{
+            System.out.println(i);
+            i=i+2;
+        }while(i<=100);
+    }
+}

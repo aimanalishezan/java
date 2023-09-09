36562d1f4e916249b4e49665cee2cd37b28eedb7
aimanalishezan <aimanalishezan22@gamil.com>
2023-09-09 23:27:49 +0600

    br

---
+++ control_statement/breakcontinue.java
@@ -0,0 +1,15 @@
+public class breakcontinue {
+    public static void main(String[] args) {
+        
+        for(int i=1;i<=100;i++){
+            if(i==10){
+                continue;//it pass the statment and run the loop 
+            }
+            else if(i>50){
+                break;//when condition match it will stop the loop
+            }
+            System.out.println(i);
+        }
+        System.out.println("***********");
+    }
+}

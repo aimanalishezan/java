d082d13cf7628df2e29dad5dc7c0fc88790e48a2
aimanalishezan <aimanalishezan22@gamil.com>
2023-09-09 19:35:17 +0600

    wow

---
+++ mathclass.java
@@ -0,0 +1,14 @@
+
+public class mathclass {
+    public static void main(String[] args) {
+        int a=10;
+        int b=-20,c;
+        
+        c=Math.max(a, b);
+        System.out.println("max is :"+c);
+        c=Math.abs(b);
+        System.out.println("the absulate value is :"+c);
+        double sqare=Math.sqrt(a);
+        System.out.printf("the absulate value is :%.0f"+sqare);
+    }
+}
